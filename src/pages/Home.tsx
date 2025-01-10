774f7de54409769370e3ba2e744447740b64b7a0
styven <styvenn16@gmail.com>
2025-01-10 10:08:57 -0300

    useSearchParams

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,7 @@ export function Home(){
             <h1>Página Home</h1>
             <nav>
                 <a href="/product">Produtos</a>
+                <a href="/product?category=tvs">Categoria</a>
             </nav>
         </div>
     )
--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,9 +1,21 @@
+import { useSearchParams } from "react-router";
+
 export function Product() {
+  const [searchParams] = useSearchParams()
+
+  const category = searchParams.get("category")
   return (
     <div>
       <h1>Pagina de Produto</h1>
       <nav>
         <a href="/">home</a>
+        {
+          category && (
+            <span>
+              Categoria <strong>{category}</strong>
+            </span>
+          )
+        }
       </nav>
     </div>
   );
