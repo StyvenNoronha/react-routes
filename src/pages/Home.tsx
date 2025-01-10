c7fe448c70da3555a8ae0b79539640b0d07fefd1
styven <styvenn16@gmail.com>
2025-01-10 10:15:37 -0300

    useNavigate

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,15 @@
+import { useNavigate } from "react-router"
+
 export function Home(){
+    const navigate = useNavigate()
     return (
         <div>
             <h1>Página Home</h1>
             <nav>
                 <a href="/product">Produtos</a>
                 <a href="/product?category=tvs">Categoria</a>
+
+                <button type="button" onClick={() => navigate("/product")}>Ver Produtos</button>
             </nav>
         </div>
     )
