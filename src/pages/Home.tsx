28ee5cbfe82717ff0ffc065aaaae51b375efe1ed
styven <styvenn16@gmail.com>
2025-01-10 09:55:24 -0300

    navegando para produtos e voltando

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,9 @@ export function Home(){
     return (
         <div>
             <h1>Página Home</h1>
+            <nav>
+                <a href="/product">Produtos</a>
+            </nav>
         </div>
     )
 }
--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,5 +1,10 @@
-export function Product(){
-    return (
-        <h1>Pagina de Produto</h1>
-    )
+export function Product() {
+  return (
+    <div>
+      <h1>Pagina de Produto</h1>
+      <nav>
+        <a href="/">home</a>
+      </nav>
+    </div>
+  );
 }
