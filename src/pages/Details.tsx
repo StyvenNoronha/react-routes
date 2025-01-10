9891831e90af35667050215be2e3656bbcb93f32
styven <styvenn16@gmail.com>
2025-01-10 10:27:48 -0300

    useParams

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,7 +1,16 @@
+import { useParams } from "react-router"
+
 export function Details(){
+    const {id} = useParams()
     return (
         <div>
             <h1>Detalhes</h1>
+            <span>
+                Id fo produtos  
+                <strong>
+                    { id}
+                </strong>
+            </span>
         </div>
     )
 }
